34599234c799a3be040e323c379aa55b46bad0f6
ohnoyuuki <k024g0074@g.neec.ac.jp>
2025-10-14 14:56:59 +0900

    t

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
-#include <Novice.h>
-
+Matrix4x4 DirectionToDirection(const Vector3& from, const Vector3& to);
 const char kWindowTitle[] = "LE2D_08_オオノ_ユウキ";
+#include <Novice.h>
+#include "KamataEngine.h"
+
+
+
 
 struct Vector3 {
 	float x;
@@ -13,10 +17,28 @@ struct Matrix4x4 {
 };
 
 
+// 正規化
+Vector3 Normalize(const Vector3& v) {
+	float length = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+	return { v.x / length, v.y / length, v.z / length };
+}
+
+
 
 
 const int kWindowWidth = 1280;
 const int kWindowHeight = 720;
+
+static const int kRowHeight = 20;
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + row * kRowHeight, "%6.02f", matrix.m[row][column], label);
+		}
+	}
+}
 
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -29,6 +51,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+
+	Vector3 from0 = Normalize(Vector3{ 1.0f,0.7f,0.5f });
+
+	Vector3 to0 = -from0;
+
+	Vector3 from1 = Normalize(Vector3{ -0.6f,0.9f,0.2f });
+
+	Vector3 to1 = Normalize(Vector3{ 0.4f,0.7f,-0.5f });
+
+	Matrix4x4 rotateMatrix0 = DirectionToDirection(Normalize(Vector3{ 1.0f,0.0f,0.0f }), Normalize(Vector3{ -1.0f,0.0f,0.0f }));
+
+	Matrix4x4 rotateMatrix1 = DirectionToDirection(from0, to0);
+
+	Matrix4x4 rotateMatrix2 = DirectionToDirection(from1, to0);
+
+
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -50,6 +89,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		MatrixScreenPrintf(0,0,rotateMatrix0,"rotateMatrix0");
+		MatrixScreenPrintf(0,kRowHeight)
 
 		///
 		/// ↑描画処理ここまで
